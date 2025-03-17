a064e8ba687ed9e50bff86ef068f853403a009a9
Eduardo Ávila <ea618369@gmail.com>
2025-03-17 09:50:22 -0300

    final

---
+++ Template_02/script.js
@@ -0,0 +1,26 @@
+var frank = document.getElementById('photoFrank')
+var tyler = document.getElementById('photoTyler')
+var kendrickGrammy = document.getElementById('photoKendrickGrammy')
+var kendrickSit = document.getElementById('photoKendrickSit')
+
+frank.addEventListener('click', function() {
+    hrefOcean = 'https://youtu.be/uzS3WG6__G4?si=Y8De-unQyvIrKwbA'
+    window.open(hrefOcean)
+});
+
+tyler.addEventListener('click', function() {
+    hrefTyler = 'https://youtu.be/dgUHE8wWhiE?si=OuheevHH_dk5dzcr'
+    window.open(hrefTyler)
+});
+
+kendrickGrammy.addEventListener('click', function() {
+    hrefKendrickGrammy = 'https://youtu.be/H58vbez_m4E?si=QiWc2_cBqIDGSCdB'
+    window.open(hrefKendrickGrammy)
+});
+
+kendrickSit.addEventListener('click', function() {
+    hrefKendrickSit = 'https://youtu.be/m-PO1_fzxVM?si=5dwF57JpKbzlP5JA'
+    window.open(hrefKendrickSit)
+});
+
+console.log("<3")
